a7e2c173af55aef7a7a407a820a7e324cf1de54e
Patrick Aleite <paleite@users.noreply.github.com>
2025-03-20 19:54:03 +0100

    Export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
+import { env } from "@/env";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Turns off typechecking
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    reactCompiler: true,
+  },
+
+  // This is needed for the build to be static
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
