d8a5844b340ee30f20e640e85c7a038c9045804b
Patrick Aleite <paleite@users.noreply.github.com>
2025-03-20 20:27:31 +0100

    Let github set up paths properly using 'configure-pages'

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Turns off typechecking
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     reactCompiler: true,
-  },
-
-  // This is needed for the build to be static
-  output: "export",
-
-  // Add basePath for GitHub Pages
-  basePath: process.env.NODE_ENV === "production" ? "/labyrinth-solver" : "",
-
-  images: {
-    unoptimized: true,
   },
 };
 
