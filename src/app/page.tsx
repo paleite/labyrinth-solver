122bddb68e135a0d2eaa893e96a325b4b474910b
Patrick Aleite <paleite@users.noreply.github.com>
2025-03-29 18:10:04 +0100

    Use less padding to avoid breaking up the logo onto multiple lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const Artifact = dynamic(() => import("@/components/artifact"), { ssr: false });
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-slate-50 p-12 antialiased">
+    <main className="min-h-screen bg-slate-50 p-6 antialiased">
       <Artifact />
       <Toaster />
     </main>
